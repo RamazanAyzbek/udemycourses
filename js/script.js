15628ad35be0def1795527fb814aa29ad98dad63
Akbi <akbiayazbai@gmail.com>
2021-11-12 21:30:27 +0600

    if file

--- js/script.js
+++ js/script.js
@@ -1,16 +1,11 @@
-const number0Films = +prompt(`Сколько фильмов вы уже сиотрели  ?`,``) ; 
-const personalMoviesDB = {
-      count : number0Films, 
-      movies : {}, 
-      actors : {}, 
-      genres : [],
-      privat : false 
-
-}; 
-const a =prompt(`Один из последних просметренных фильмов ? `, ``),
-      b = prompt(`На сколько оцените его ? `, ``), 
-      c =prompt(`Один из последних просметренных фильмов ? `, ``),
-      d = prompt(`На сколько оцените его ? `, ``);
-      personalMoviesDB.movies[a]  =b; 
-      personalMoviesDB.movies[c] =d ;
-      document.write(personalMoviesDB); 
+`use strict`; 
+const num =90;
+//(num===50) ? console.log(`ok !`) : console.log(`no !`); 
+switch(num){
+      case 39 : console.log(`no `); 
+      break; 
+      case 50 : console.log(`yes !`); 
+      break; 
+      default : console.log(`no - yes !`); 
+      break; 
+}
