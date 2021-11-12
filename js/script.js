8c05461728abc59d0619622529aac59389b86e98
Akbi <akbiayazbai@gmail.com>
2021-11-12 21:47:49 +0600

    first commit

--- js/script.js
+++ js/script.js
@@ -1,11 +1,1 @@
 `use strict`; 
-const num =90;
-//(num===50) ? console.log(`ok !`) : console.log(`no !`); 
-switch(num){
-      case 39 : console.log(`no `); 
-      break; 
-      case 50 : console.log(`yes !`); 
-      break; 
-      default : console.log(`no - yes !`); 
-      break; 
-}
