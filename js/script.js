a199e8da3b6611d9d6cb0baa4008e9f945e8ee08
Akbi <akbiayazbai@gmail.com>
2021-11-13 10:48:52 +0600

    function js

--- js/script.js
+++ js/script.js
@@ -1,43 +1,27 @@
 `use strict`; 
-const number0Films = prompt(`Сколько фильмов вы уже смотрели ?`, ``); 
-const personalMovieDB = {
-      count : number0Films,
-      movies : {}, 
-      actors : {}, 
-      genres: [], 
-      privat : false 
-}; 
+let num =10; 
+function showFirstMessage(text){ 
+      console.log(text);
+      num =4;  
+}
+showFirstMessage("Hello World !");
+console.log(num);  
+function calc(a,b){
+      return(a+b); 
+}
 
-
-       for(let i=0; i<2; i++) {
-           const  a = prompt(`Один из последних просмотренных фильмов`, ``), 
-            b = prompt(`На сколько оцените его ? `, ``); 
-            if(a!=null && b!=null && a!=`` && b!=`` && a.length<50)
-            {
-               personalMovieDB.movies[a] =b;
-               console.log(`done`) ; 
-            }
-            else  {
-                  console.log(`error`); 
-                  --i; 
-            }
-           
-
-       }
-       if(personalMovieDB.count<10) {
-             console.log(`Просмотрено довольно мало фильмов`); 
-
-       } else if(personalMovieDB.count>10 && personalMovieDB.count<30)
-       {
-           console.log(`Вы классический зритель `); 
-       } else if(personalMovieDB.count>=30)
-       {
-             console.log(`Вы просто киноман`); 
-       }else { 
-             console.log(`Произошло ошибка`); 
-       }
-
-
-       console.log(personalMovieDB); 
+console.log(calc(4,6)); 
+console.log(calc(5,6)); 
        
-       
+function ret(){
+      let num =50; 
+      return num; 
+}
+const anotherNum = ret(); 
+console.log(anotherNum); 
+const logger = function(){
+      console.log(`Hello`); 
+}
+const logg = (a,b) =>  a+b; 
+
+
