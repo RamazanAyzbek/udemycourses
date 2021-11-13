138caf581009d462eb19427337500f3c020c17ba
Akbi <akbiayazbai@gmail.com>
2021-11-13 09:15:06 +0600

    films 2 code

--- js/script.js
+++ js/script.js
@@ -1,2 +1,43 @@
 `use strict`; 
-console.log(`hitri`); 
+const number0Films = prompt(`Сколько фильмов вы уже смотрели ?`, ``); 
+const personalMovieDB = {
+      count : number0Films,
+      movies : {}, 
+      actors : {}, 
+      genres: [], 
+      privat : false 
+}; 
+
+
+       for(let i=0; i<2; i++) {
+           const  a = prompt(`Один из последних просмотренных фильмов`, ``), 
+            b = prompt(`На сколько оцените его ? `, ``); 
+            if(a!=null && b!=null && a!=`` && b!=`` && a.length<50)
+            {
+               personalMovieDB.movies[a] =b;
+               console.log(`done`) ; 
+            }
+            else  {
+                  console.log(`error`); 
+                  --i; 
+            }
+           
+
+       }
+       if(personalMovieDB.count<10) {
+             console.log(`Просмотрено довольно мало фильмов`); 
+
+       } else if(personalMovieDB.count>10 && personalMovieDB.count<30)
+       {
+           console.log(`Вы классический зритель `); 
+       } else if(personalMovieDB.count>=30)
+       {
+             console.log(`Вы просто киноман`); 
+       }else { 
+             console.log(`Произошло ошибка`); 
+       }
+
+
+       console.log(personalMovieDB); 
+       
+       
